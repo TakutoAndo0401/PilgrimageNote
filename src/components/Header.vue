b6df2547af2107efa170afe6bbf4d69d1ac560ab
RitaChan0401 <terminus_rita@yahoo.co.jp>
2019-11-18 00:41:31 +0900

    unfinished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,17 +5,17 @@
         <v-flex xs12><span id="title">聖地巡礼ノート</span></v-flex>
       </v-toolbar-title>
       <v-spacer />
-      <v-icon id="icon" style="font-size: 20px">fas fa-search</v-icon>
-      <v-flex xs4 md4>
-      <v-row>
-        <v-text-field
-                color="white"
-                hide-details
-                placeholder="Search..."
-                single-line
-        />
-      </v-row>
-      </v-flex>
+<!--      <v-icon id="icon" style="font-size: 20px">fas fa-search</v-icon>-->
+<!--      <v-flex xs4 md4>-->
+<!--      <v-row>-->
+<!--        <v-text-field-->
+<!--                color="white"-->
+<!--                hide-details-->
+<!--                placeholder="Search..."-->
+<!--                single-line-->
+<!--        />-->
+<!--      </v-row>-->
+<!--      </v-flex>-->
     </v-app-bar>
   </v-app>
 </template>
