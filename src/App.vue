b6520b34802d3a4dd41666fce10c66f474aa73c6
RitaChan0401 <terminus_rita@yahoo.co.jp>
2019-11-21 13:18:31 +0900

    fix loading display

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <Header/>
-    <ArticleCard/>
+    <div ref="loading" id="loading">
+      <img src="https://www.wp-master.club/wp-content/uploads/2018/10/loading.gif" alt="loading">
+    </div>
+    <div ref="app" class="hidden">
+      <Header/>
+      <ArticleCard/>
+    </div>
   </div>
 </template>
 
@@ -14,5 +19,31 @@
       Header,
       ArticleCard,
     },
+    mounted() {
+      let loading =  this.$refs.loading;
+      let app = this.$refs.app;
+      window.addEventListener('load', function () {
+        loading.style.display = 'none';
+        app.classList.remove('hidden');
+      });
+    }
   };
 </script>
+
+<style>
+  #loading {
+    width: 100%;
+    height: 100vh;
+    background-color: #fff;
+    position: relative;
+  }
+  #loading img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .hidden {
+    display: none;
+  }
+</style>
